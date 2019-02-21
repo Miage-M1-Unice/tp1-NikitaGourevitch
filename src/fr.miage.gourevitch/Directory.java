8659b74ea40d8e627d389f5130f01d9b35148f01
Gourevitch Nikita <aonn.ru@gmail.com>
2019-02-21 20:41:23 +0100

    Add Exercice 1.c

--- src/fr.miage.gourevitch/Directory.java
+++ src/fr.miage.gourevitch/Directory.java
@@ -1,8 +1,10 @@
 package fr.miage.gourevitch;
+
 import java.io.FilenameFilter;
 import java.io.File;
 import java.util.Objects;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Directory {
 
@@ -22,9 +24,9 @@ public class Directory {
         }
     }
 
-   /*
-   Dans une classe interne nommée
-    */
+    /*
+    Dans une classe interne nommée
+     */
     public void toStringFileInterne(String pathFile) {
 
         for (File file : Objects.requireNonNull(new File(pathFile).listFiles(new Filtre2()))) {
@@ -40,20 +42,19 @@ public class Directory {
 
     }
 
-   /*
-   Dans une classe interne anonyme (anonymous inner class) en argument de la méthode listFiles() (une « instanciation » d'une interface),
-    */
+    /*
+    Dans une classe interne anonyme (anonymous inner class) en argument de la méthode listFiles() (une « instanciation » d'une interface),
+     */
     public void toStringFileAnonyme(String pathFile) {
 
         for (File file : Objects.requireNonNull(new File(pathFile).listFiles(
                 new FilenameFilter() {
                     @Override
                     public boolean accept(File directory, String name) {
-                            File f = new File(directory.getAbsolutePath() + "/" + name);
-                            return f.isDirectory() || name.endsWith(".java");
+                        File f = new File(directory.getAbsolutePath() + "/" + name);
+                        return f.isDirectory() || name.endsWith(".java");
                     }
-                })))
-        {
+                }))) {
 
             if (file.isDirectory()) {
                 System.out.println(file.toString());
@@ -66,7 +67,31 @@ public class Directory {
 
     }
 
-    private class Filtre2 implements FilenameFilter {
+   public  void exerciceC(){
+
+        String regex = ".*class";
+        String regexA = ".*\\$.*";
+        String regexB = ".*\\$[0-9]*\\.class";
+
+        Pattern p = Pattern.compile(regex);
+        Pattern pA = Pattern.compile(regexA);
+        Pattern pB = Pattern.compile(regexB);
+
+        String toMatch = "classclassclass";
+        String toMatchA = "classcla$ssclass";
+        String toMatchB = "clasvsclassclass9";
+
+        Matcher match = p.matcher(toMatch);
+        Matcher matchA = p.matcher(toMatchA);
+        Matcher matchB = p.matcher(toMatchB);
+
+    }
+
+
+
+
+
+     class Filtre2 implements FilenameFilter {
 
         private String regex;
 
